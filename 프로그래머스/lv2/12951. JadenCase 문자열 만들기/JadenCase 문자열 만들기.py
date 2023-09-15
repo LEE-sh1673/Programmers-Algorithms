48144f9b4c7ba4183a748f79ad09d2644bf700f0
SeungHoon Lee <lsh901673@gmail.com>
2023-09-15 23:45:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -3,6 +3,12 @@ def solution(s):
 
 
 def jaden_case(s):
-    if not s or not s.strip(): return s
+    if is_empty(s):
+        return s
+    
     first, second = s[0], s[1:]
     return s.lower() if not first.isalpha() else first.upper() + second.lower()
+
+
+def is_empty(s):
+    return not s or not s.strip()
