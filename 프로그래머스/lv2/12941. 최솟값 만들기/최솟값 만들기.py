10b9f7e29027f55397075e351efccec4e6e556b3
SeungHoon Lee <lsh901673@gmail.com>
2023-09-16 00:50:11 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,6 +1,2 @@
-from itertools import permutations
-from functools import reduce
-
-
 def solution(A, B):
-    return sum([a * b for a, b, in zip(sorted(A), sorted(B, reverse=True))])
+    return sum([a * b for a, b in zip(sorted(A), sorted(B, reverse=True))])
