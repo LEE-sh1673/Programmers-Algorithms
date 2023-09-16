317af3fc9c751292e4f675a8c6074377de69b7e9
SeungHoon Lee <lsh901673@gmail.com>
2023-09-16 18:13:27 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,9 +1,11 @@
 def solution(s):
-    transforms, removals = 0, 0
+    removals, transforms = 0, 0
 
     while s != '1':
         removals += (len(s) - s.count('1'))
-        s = bin(s.count('1'))[2:]
         transforms += 1
+
+        nums_one = s.count('1')
+        s = bin(nums_one)[2:]
 
     return [transforms, removals]
