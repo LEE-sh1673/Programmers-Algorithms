55e195273277b845d943403ca3f6465c3233a8c4
SeungHoon Lee <lsh901673@gmail.com>
2023-09-16 21:22:58 +0900

    [level 2] Title: 숫자의 표현, Time: 3.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,10 +1,15 @@
 def solution(n):
     answer = 0
-
-    for i in range(1, (n + 1) // 2 + 1):
+    
+    if n < 2:
+        return n
+    
+    k = (n + 1) // 2 + 1
+    
+    for i in range(1, k):
         total = 0
 
-        for j in range(i, (n + 1) // 2 + 1):
+        for j in range(i, k):
             total += j
 
             if total >= n:
