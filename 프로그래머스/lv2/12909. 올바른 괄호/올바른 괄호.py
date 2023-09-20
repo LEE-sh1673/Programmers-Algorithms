195b99f1ad99f0fe11c0c7af1f4fea1513da59a4
SeungHoon Lee <lsh901673@gmail.com>
2023-09-20 22:03:12 +0900

    [level 2] Title: 올바른 괄호, Time: 6.72 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -3,8 +3,7 @@ def solution(s):
 
     for ch in s:
         parens = parens + 1 if ch == '(' else parens - 1
-
-        if parens < 0:
-            break
+    
+        if parens < 0: return False
 
     return parens == 0
