4e61155fc2bba25c691ac54ef4272cad06413c3a
SeungHoon Lee <lsh901673@gmail.com>
2023-09-23 16:53:56 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,14 +1,8 @@
-from math import sqrt, ceil
-
-
 def solution(brown, yellow):
-    w = max(quadratic(2, -(brown - 4), 2*yellow))
-    h = yellow // w
-    return [w + 2, h + 2]
+    br = brown + yellow
 
+    for w in range(2, br + 1):
+        h = br // w
 
-def quadratic(a, b, c):
-    d = (b ** 2) - (4 * a * c)
-    sol1 = ceil((-b + sqrt(d)) / (2 * a))
-    sol2 = ceil((-b - sqrt(d)) / (2 * a))
-    return sol1, sol2
+        if (w - 2) * (h - 2) == yellow:
+            return sorted([w, h], reverse=True)
