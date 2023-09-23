7d682e8d069c1c13a48d243cec6e755b668212e7
SeungHoon Lee <lsh901673@gmail.com>
2023-09-23 11:34:41 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,11 +1,8 @@
+
 def solution(n):
     n_ones = nums_one(n)
     return next(i for i in range(n + 1, 2 * n + 1) if n_ones == nums_one(i))
 
 
 def nums_one(num):
-    return dec_to_bin(num).count('1')
-
-
-def dec_to_bin(num):
-    return bin(num).replace('0b', '')
+    return bin(num).count('1')
