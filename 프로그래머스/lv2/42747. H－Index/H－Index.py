493894a3ad99c8338556cd2bb6900d0bb58f4a0e
SeungHoon Lee <lsh901673@gmail.com>
2023-10-06 17:57:56 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,3 +1,13 @@
 def solution(citations):
     citations.sort(reverse=True)
-    return next((i for i, citation in enumerate(citations) if i >= citation), len(citations))
+    
+    h = 0
+    
+    for citation in citations:
+        if h >= citation:
+            break
+        h += 1
+    
+    return h
+    
+    # return next((h for h, citation in enumerate(citations) if h >= citation), len(citations))
