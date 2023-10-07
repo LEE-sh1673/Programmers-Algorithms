ef6afc25f1064ef94b9d51a4358df8d2b646939b
SeungHoon Lee <lsh901673@gmail.com>
2023-10-07 15:46:37 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.67 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,11 +1,10 @@
 def solution(arr1, arr2):
     answer = []
-    
-    for i in range(len(arr1)):
+
+    for arr1_col in arr1:
         k_arr = []
-        for j in range(len(arr2[0])):
-            total = sum([arr1[i][k] * arr2[k][j] for k in range(len(arr1[0]))])
-            k_arr.append(total)
+        for arr2_row in zip(*arr2):
+            k_arr.append(sum([el1 * el2 for el1, el2 in zip(arr1_col, arr2_row)]))
         answer.append(k_arr)
-        
+
     return answer
