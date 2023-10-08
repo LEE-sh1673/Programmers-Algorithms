1c853b7a03e9e540a48cae6c175969219b1ec7af
SeungHoon Lee <lsh901673@gmail.com>
2023-10-08 23:36:59 +0900

    [level 2] Title: [1차] 캐시, Time: 1.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -4,22 +4,22 @@ LV2. [1차] 캐시
 
 풀이:
 1. 도시 이름을 대문자 or 소문자로 변환한다.
-2. 도시와 캐시에 대해 다음을 진행한다.
-	
+2. 각 도시에 대해 다음을 진행한다.
+
   A. cache hit:
-    - 실행 시간을 +1한다.
     - cache hit 된 도시가 캐시의 뒤(rear)에 오도록 순서를 조정한다.
+    - 실행 시간을 +1한다.
     
   B. cache miss:
-    - 실행 시간을 +5한다.
     - 캐시의 뒤(rear)에 도시를 추가한다.
+    - 캐시 크기에 따라 다음을 진행한다.
 
-3. 캐시 크기에 따라 다음을 진행한다.
+      B.A len(캐시) > cacheSize:
+        - 캐시의 앞(front)에 있는 도시를 삭제한다.
+
+    - 실행 시간을 +5한다.
   
-  3.1 len(캐시) >= cacheSize:
-    - 캐시의 앞(front)에 있는 도시를 삭제한다.
-  
-4. 계산된 실행 시간을 반환한다.
+3. 계산된 실행 시간을 반환한다.
 """
 
 
@@ -27,34 +27,36 @@ class CacheStat:
     CACHE_HIT = 1
     CACHE_MISS = 5
 
-    
+
 class ExecutionTimeMeter(CacheStat):
     def __init__(self, cache_size):
         self.cache_size = cache_size
         self.cache = []
-    
+
     def meter(self, items):
         return sum([self.execute(item.upper()) for item in items])
-    
+
     def execute(self, item):
-        if self.is_hit(item):
-            self.move_to_rear(item)
-            return self.CACHE_HIT
-        
-        self.cache.append(item)
-        self.update_cache()
-        return self.CACHE_MISS
-        
+        hit = self.is_hit(item)
+        self.update_cache(item, hit)
+        return self.CACHE_HIT if hit else self.CACHE_MISS
+
     def is_hit(self, item):
         return item in self.cache
-    
+
+    def update_cache(self, item, hit):
+        if hit:
+            self.move_to_rear(item)
+            return
+
+        self.cache.append(item)
+
+        if len(self.cache) > self.cache_size:
+            self.cache.pop(0)
+
     def move_to_rear(self, item):
         if (idx := self.cache.index(item)) >= 0:
             self.cache.append(self.cache.pop(idx))
-		
-    def update_cache(self):
-        if len(self.cache) > self.cache_size:
-            self.cache.pop(0)        
 
 
 def solution(cacheSize, cities):
