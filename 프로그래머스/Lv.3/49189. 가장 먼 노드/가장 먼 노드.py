c2b045e1b31a449f2e0d3816ff594d77d264486c
SeungHoon Lee <lsh901673@gmail.com>
2024-01-14 00:05:35 +0900

    [level Lv.3] Title: 가장 먼 노드, Time: 90.91 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/Lv.3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/Lv.3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -13,25 +13,22 @@ def solution(N, road):
         graph[a].append((b, 1))
         graph[b].append((a, 1))
 
-    def dijkstra(start):
-        nonlocal max_dist
-        q = []
-        heapq.heappush(q, (0, start))
-        distance[start] = 0
+    q = []
+    heapq.heappush(q, (0, 1))
+    distance[1] = 0
 
-        while q:
-            dist, now = heapq.heappop(q)
+    while q:
+        dist, now = heapq.heappop(q)
 
-            if distance[now] < dist:
-                continue
+        if distance[now] < dist:
+            continue
 
-            for to, cost in graph[now]:
-                cost_total = dist + cost
+        for to, cost in graph[now]:
+            cost_total = dist + cost
 
-                if cost_total < distance[to]:
-                    distance[to] = cost_total
-                    heapq.heappush(q, (cost_total, to))
-                    max_dist = max(max_dist, cost_total)
+            if cost_total < distance[to]:
+                distance[to] = cost_total
+                heapq.heappush(q, (cost_total, to))
+                max_dist = max(max_dist, cost_total)
 
-    dijkstra(1)
-    return len([dist for dist in distance if dist == max_dist])
+    return distance.count(max_dist)
