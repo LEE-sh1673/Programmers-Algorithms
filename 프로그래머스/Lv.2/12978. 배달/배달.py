dc77eaad472c1f61a82cdeb659f26beed2c4acf3
SeungHoon Lee <lsh901673@gmail.com>
2024-01-14 00:25:20 +0900

    [level Lv.2] Title: 배달, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12978. 배달/배달.py
+++ 프로그래머스/Lv.2/12978. 배달/배달.py
@@ -1,34 +1,57 @@
-import heapq
+# import heapq
 
 INF = int(1e9)
+
+
+# def solution(N, road, K):
+#     graph = [[] for _ in range(N + 1)]
+#     distance = [INF] * (N + 1)
+
+#     for info in road:
+#         a, b, c = info
+#         graph[a].append((b, c))
+#         graph[b].append((a, c))
+
+#     q = []
+#     heapq.heappush(q, (0, 1))
+#     distance[1] = 0
+
+#     while q:
+#         dist, now = heapq.heappop(q)
+
+#         if distance[now] < dist:
+#             continue
+
+#         for to, cost in graph[now]:
+#             cost_total = dist + cost
+
+#             if cost_total < distance[to]:
+#                 distance[to] = cost_total
+#                 heapq.heappush(q, (cost_total, to))
+
+#     return len([dist for dist in distance if dist != INF and dist <= K])
 
 
 def solution(N, road, K):
     graph = [[] for _ in range(N + 1)]
     distance = [INF] * (N + 1)
 
-    for info in road:
-        a, b, c = info
+    for a, b, c in road:
         graph[a].append((b, c))
         graph[b].append((a, c))
 
-    def dijkstra(start):
-        q = []
-        heapq.heappush(q, (0, start))
-        distance[start] = 0
+    q = [(1, 0)]
+    distance[1] = 0
 
-        while q:
-            dist, now = heapq.heappop(q)
+    while q:
+        now, dist = q.pop(0)
 
-            if distance[now] < dist:
-                continue
+        for adj_node, adj_dist in graph[now]:
+            cost = dist + adj_dist
 
-            for to, cost in graph[now]:
-                cost_total = dist + cost
+            if cost < distance[adj_node]:
+                distance[adj_node] = cost
+                q.append((adj_node, cost))
 
-                if cost_total < distance[to]:
-                    distance[to] = cost_total
-                    heapq.heappush(q, (cost_total, to))
-
-    dijkstra(1)
+    print(distance)            
     return len([dist for dist in distance if dist != INF and dist <= K])
