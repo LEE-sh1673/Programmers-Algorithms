f0dbe2714a39f1a9ba042afca574f9424ffb417c
SeungHoon Lee <lsh901673@gmail.com>
2024-01-28 22:52:25 +0900

    [level 2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,33 +1,3 @@
-# def solution(s):
-#     s_length = len(s)
-#     min_length = s_length
-    
-#     # 문자열의 길이가 1 또는 0인 경우 그대로 반환합니다.
-#     if s_length == 1 or s_length == 0:
-#         return s_length
-    
-#     # 문자열 길이를 1 ~ N/2 까지 압축시키고 그 길이를 저장합니다.
-#     for tok_len in range(1, s_length//2 + 1):
-#         words = [s[i:i+tok_len] for i in range(0, s_length, tok_len)]
-#         compressed = []
-#         curr_count = 1
-        
-#         for A, B in zip(words, words[1:] + ['']):
-#             if A == B:
-#                 curr_count += 1
-#             else:
-#                 compressed.append([curr_count, A])
-#                 curr_count = 1
-                
-#         compressed_length = sum((len(str(count)) if count != 1 else 0) + len(word)  for count, word in compressed)
-
-#         if min_length >= compressed_length:
-#             min_length = compressed_length
-    
-#     # 가장 작은 길이를 반환합니다.
-#     return min_length
-
-
 def solution(s):
     str_length = len(s)
 
