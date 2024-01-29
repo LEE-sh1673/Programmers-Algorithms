4c54592b57a0ed1a54243490e01b816abdeefa4e
SeungHoon Lee <lsh901673@gmail.com>
2024-01-29 21:27:58 +0900

    [level 2] Title: 타겟 넘버, Time: 2.97 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,15 @@
-"""
-타겟 넘버
-https://school.programmers.co.kr/learn/courses/30/lessons/43165
-"""
-
-
 def solution(numbers: list, target: int):
     answer = 0
 
-    def dfs(r, index):
+    def dfs(total, index):
+        nonlocal answer
+        
         if index == len(numbers):
-            if r == target:
-                nonlocal answer
+            if total == target:
                 answer += 1
         else:
-            dfs(r + numbers[index], index + 1)
-            dfs(r - numbers[index], index + 1)
+            dfs(total + numbers[index], index + 1)
+            dfs(total - numbers[index], index + 1)
 
-    dfs(0, 0)
+    t = dfs(0, 0)
     return answer
