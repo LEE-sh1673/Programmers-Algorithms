c07ec17c04ad81d0baadb8db5166b734701a10c4
SeungHoon Lee <lsh901673@gmail.com>
2024-01-31 22:41:24 +0900

    [level 2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,7 +1,23 @@
-def solution(s):
-    str_length = len(s)
+"""
+"aabbaccc" -> "aa", "bb", "a", "ccc" -> "2a2ba3c"
 
-    if str_length <= 1:
+문자를 1개 단위로 자르는 경우:
+aa => 2
+ab => 2a
+bb => 2 
+ba => 2b
+ac => 1a
+cc => 2
+cc => 3
+c => 3c
+
+"ababcdcdababcdcd" -> "abab", "cdcd", "abab" -> "cdcd" -> "2ab2cd2ab2cd"
+"ababcdcdababcdcd" -> "ababcdcd / ababcdcd" -> "2ababcdcd"
+
+- 이때 최대로 자를 수 있는 단위는 len(s)/2
+""" 
+def solution(s):
+    if (str_length := len(s)) <= 1:
         return str_length
 
     min_length = str_length
@@ -18,9 +34,7 @@ def solution(s):
                 res.append([curr_word, curr_cnt])
                 curr_cnt = 1
 
-        curr_length = sum([len(word) + (len(str(cnt)) if cnt != 1 else 0) for word, cnt in res])
-
-        if curr_length < min_length:
-            min_length = curr_length
+        curr_length = sum([len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res])
+        min_length = min(min_length, curr_length)
 
     return min_length
