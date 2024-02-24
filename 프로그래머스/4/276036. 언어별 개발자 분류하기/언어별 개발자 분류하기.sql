c96a9b5d29752044c0802c89020c44dbed6c1650
SeungHoon Lee <lsh901673@gmail.com>
2024-02-24 23:15:33 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -3,7 +3,8 @@ SELECT
         WHEN SUM(CASE WHEN category = 'Front End' THEN 1 ELSE 0 END) > 0 
             AND SUM(CASE WHEN name = 'Python' THEN 1 ELSE 0 END) > 0 THEN 'A'
         WHEN SUM(CASE WHEN name = 'C#' THEN 1 ELSE 0 END) > 0 THEN 'B'
-        WHEN SUM(CASE WHEN category = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C' 
+        WHEN SUM(CASE WHEN category = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C'
+        ELSE 'NONE'
     END AS grade,
     id,
     email
@@ -12,12 +13,12 @@ FROM
     JOIN (
         SELECT name, code, category
         FROM SKILLCODES
-        WHERE category = "Front End" or name in ('C#', 'Python')
-    ) as S 
-    on D.skill_code & S.code > 0
+        WHERE category = "Front End" OR name IN ('C#', 'Python')
+    ) AS S 
+    ON D.skill_code & S.code > 0
 GROUP BY
     id, email
 HAVING
-    grade IS NOT NULL
+    grade != 'NONE'
 ORDER BY
     grade, id;
