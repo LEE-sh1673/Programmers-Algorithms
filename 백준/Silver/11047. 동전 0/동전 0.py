cfbf815bd67be2824f9523834903fc1fdcf49ffc
SeungHoon Lee <lsh901673@gmail.com>
2024-03-04 23:07:31 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,22 @@
+"""
+11047. 동전 0
+"""
+from sys import stdin
+
+input = stdin.readline
+
+
+def solution(n, k):
+    answer = 0
+    coins = [int(input()) for _ in range(n)]
+
+    for coin in coins[::-1]:
+        if coin <= k:
+            answer += k // coin
+            k %= coin
+
+    return answer
+
+
+n, k = map(int, input().split())
+print(solution(n, k))
