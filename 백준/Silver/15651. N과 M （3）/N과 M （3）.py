0de70f24b82225f9b24020fe8812edef272218c5
SeungHoon Lee <lsh901673@gmail.com>
2024-03-14 01:04:39 +0900

    [Silver III] Title: N과 M (3), Time: 1240 ms, Memory: 102648 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,23 +1,7 @@
-"""
-15651. N과 M (3)
-"""
-from sys import stdin
-
-input = stdin.readline
-
-
 def solution(n, m):
-    visited = []
-
-    def dfs():
-        if len(visited) == m:
-            print(*visited)
-            return
-        for number in range(1, n + 1):
-            visited.append(number)
-            dfs()
-            visited.pop()
-    dfs()
+    from itertools import product
+    products = map(lambda nums: " ".join(map(str, nums)), product(range(1, n+1), repeat=m))
+    print(*products, sep='\n')
 
 
 n, m = map(int, input().split())
