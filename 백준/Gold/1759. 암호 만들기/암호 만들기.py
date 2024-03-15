841cd6f9daced747697619897917a6837d6370df
SeungHoon Lee <lsh901673@gmail.com>
2024-03-15 17:17:57 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -9,25 +9,23 @@ def solution(l, c):
     words.sort()
 
     def is_candidate(pwd):
-        vowels = len([ch for ch in pwd if ch in "aeiou"])
-        return vowels >= 1 and (l - vowels) >= 2
+        vowels = len([ch for ch in pwd if ch in 'aeiou'])
+        return vowels > 0 and (l - vowels) >= 2
 
-    def dfs(pwd):
+    def dfs(start, pwd):
         if len(pwd) == l and is_candidate(pwd):
-            print("".join(pwd))
+            print(pwd)
             return
 
-        for i in range(c):
+        for i in range(start, c):
             if visited[i] or (pwd and pwd[-1] >= words[i]):
                 continue
 
             visited[i] = True
-            pwd.append(words[i])
-            dfs(pwd)
+            dfs(start + 1, pwd + words[i])
             visited[i] = False
-            pwd.remove(words[i])
 
-    dfs([])
+    dfs(0, '')
 
 
 l, c = map(int, input().split())
