a92ccd81c7deb3bda290da073c038512d5440626
SeungHoon Lee <lsh901673@gmail.com>
2024-03-19 21:38:38 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -9,7 +9,6 @@ def solution():
         
         while que:
             i, j = que.pop(0)
-            visited.append((i,j))
         
             for dir_x, dir_y in [(-1,0),(0,1),(1,0),(0,-1)]:
                 x = i + dir_x
@@ -18,26 +17,23 @@ def solution():
                 if x < 0 or x > n-1 or y < 0 or y > m-1:
                     continue
             
-                if (x, y) in visited or fields[x][y] == 0:
-                    continue
-                
-                visited.append((x, y))
-                que.append((x, y))
-    
+                if fields[x][y] == 1:
+                    que.append((x, y))
+                    fields[x][y] = 0
+            
 
     for _ in range(int(input())):
-        m, n, k = map(int, input().split())
-        fields = [[0 for _ in range(m)] for _ in range(n)]
+        n, m, k = map(int, input().split())
+        fields = [[0] * m for _ in range(n)]
         answer = 0
-        visited = []
 
         for _ in range(k):
             i, j = map(int, input().split())
-            fields[j][i] = 1
+            fields[i][j] = 1
     
         for i in range(n):
             for j in range(m):
-                if fields[i][j] and (i, j) not in visited:
+                if fields[i][j]:
                     answer += 1
                     dfs(i, j)
     
