b038ed9f4a8ba46914612e4ab3a930acdfaef6de
SeungHoon Lee <lsh901673@gmail.com>
2024-03-22 01:41:33 +0900

    [Gold III] Title: 아기 상어, Time: 164 ms, Memory: 34156 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,23 +1,5 @@
 """
 16236. 아기 상어
-
-- 아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없다.
-- 아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 있다.
-- 아기 상어는 자신의 크기와 같은 물고기를 먹을 수는 없지만, 칸을 지날 수 있다.
-
-[행동 방식]
-1. 더 이상 먹을 수 있는 물고기가 없다면 엄마 상어에게 도움을 요청
-2. 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
-3. 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
-4.
-    4-1. 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기를 먹음
-    4-2. 그러한 물고기가 여러 마리라면, 가장 왼쪽에 있는 물고기를 먹음
-
-
-(거리) = 아기 상어 칸 ~ 물고기 칸 이동 시, 지나야하는 칸의 개수의 최솟값
-아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가한다.
-
-아기 상어가 몇 초 동안 엄마 상어에게 도움을 요청하지 않고 물고기를 잡아먹을 수 있는지 구하는 프로그램을 작성하시오.
 """
 from collections import deque
 from sys import stdin
@@ -60,54 +42,56 @@ class Shark:
             self.size += 1
 
 
-def bfs(i, j):
-    fishes = []
-    queue = deque()
-    visited = [[False] * n for _ in range(n)]
+def find_shark():
+    for i in range(n):
+        for j in range(n):
+            if space[i][j] == 9:
+                return Shark(i, j)
 
-    queue.append((i, j, 0))
-    visited[i][j] = True
 
-    while queue:
-        i, j, cnt = queue.popleft()
+def solution(n, space, shark):
+    def bfs(i, j):
+        fishes = []
+        queue = deque()
+        visited = [[False] * n for _ in range(n)]
 
-        for dir_x, dir_y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            x = i + dir_x
-            y = j + dir_y
+        queue.append((i, j, 0))
+        visited[i][j] = True
 
-            if x < 0 or x > n - 1 or y < 0 or y > n - 1:
-                continue
+        while queue:
+            i, j, cnt = queue.popleft()
 
-            if not visited[x][y] and space[x][y] <= shark.size:
-                visited[x][y] = True
-                queue.append((x, y, cnt + 1))
+            for dir_x, dir_y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                x = i + dir_x
+                y = j + dir_y
 
-                if 0 < space[x][y] < shark.size:
-                    fishes.append(Fish(x, y, cnt + 1))
+                if x < 0 or x > n - 1 or y < 0 or y > n - 1:
+                    continue
 
-    return sorted(fishes, key=lambda fish: (fish.dist, fish.x, fish.y))
+                if not visited[x][y] and space[x][y] <= shark.size:
+                    visited[x][y] = True
+                    queue.append((x, y, cnt + 1))
+
+                    if 0 < space[x][y] < shark.size:
+                        fishes.append(Fish(x, y, cnt + 1))
+
+        return sorted(fishes, key=lambda fish: (fish.dist, fish.x, fish.y))
+
+    while True:
+        candidate_fishes = bfs(shark.x, shark.y)
+
+        if not candidate_fishes:
+            break
+
+        fish = candidate_fishes[0]
+        space[shark.x][shark.y] = 0
+        space[fish.x][fish.y] = 0
+        shark.eat(fish)
+
+    print(shark.distance)
 
 
 n = int(input())
 space = [list(map(int, input().strip().split())) for _ in range(n)]
-shark = Shark()
-
-for i in range(n):
-    for j in range(n):
-        if space[i][j] == 9:
-            shark.x = i
-            shark.y = j
-            break
-
-while True:
-    candidate_fishes = bfs(shark.x, shark.y)
-
-    if not candidate_fishes:
-        break
-
-    fish = candidate_fishes[0]
-    space[shark.x][shark.y] = 0
-    space[fish.x][fish.y] = 0
-    shark.eat(fish)
-
-print(shark.distance)
+shark = find_shark()
+solution(n, space, shark)
