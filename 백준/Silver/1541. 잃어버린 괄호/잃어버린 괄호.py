b92deca23cd431b5a09bb48f0cf7a5bb8cf5f60c
SeungHoon Lee <lsh901673@gmail.com>
2024-03-28 22:30:49 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,20 +1,17 @@
-"""
-- 가장 처음과 마지막은 숫자, 즉 첫 번째 숫자는 무조건 양수가 나옴
-- 최소값을 구하기 위해 처음 '-'를 기준으로 끊는다.
-- 이후 각 그룹 별 합을 구하고 뺄셈 연산을 진행한다.
-
-e.g. 10-20-30+40
-1. '-'를 기준으로 끊기 => 10, 20, 30+40
-2. 각 그룹의 합 구하기 => 10, 20, 70
-3. 뺄셈 연산 진행 => 10 - 20 - 70 = -80
-"""
 from sys import stdin
 
+input = stdin.readline
+exprs = input().strip()
+answer = 0
 
-formula = stdin.readline().split('-')
-answer = sum([int(el) for el in formula[0].split('+')])
+# 1. "-"을 기준으로 분리한다.
+first_expr, *exprs = exprs.split("-")
 
-for stmt in formula[1:]:
-    answer -= sum([int(el) for el in stmt.split('+')])
-    
+# 2. 첫 번째 요소와 나머지 요소들로 분리한다.
+answer = sum(map(int, first_expr.split("+")))
+
+# 3. 첫 번째 요소와 제외한 나머지 요소들의 합을 구하고 -1을 곱한다.
+for expr in exprs:
+    answer += -1 * sum(map(int, expr.split("+")))
+
 print(answer)
