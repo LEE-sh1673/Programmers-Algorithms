3e31911051f8d59fb955d5d3f1aad0d1239deac6
SeungHoon Lee <lsh901673@gmail.com>
2024-03-29 23:50:39 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -1,12 +1,35 @@
 from sys import stdin
 
-n = int(stdin.readline().strip())
-nums = list(map(int, stdin.readline().strip().split()))
+input = stdin.readline
+n = int(input().strip())
+nums = list(map(int, input().split()))
+check = []
 dp = [1] * n
 
+
+def lower_bound(x):
+    start, end = 0, len(check) - 1
+    ans = 0
+    
+    while True:
+        if start > end:
+            return ans
+            
+        mid=(start+end)//2
+
+        if check[mid] <= x:
+            ans=mid
+            end=mid-1
+        else:
+             start=mid+1
+
+
 for i in range(n):
-    for j in range(i):
-        if nums[j] > nums[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    if not check or check[-1] > nums[i]:
+        check.append(nums[i])
+    else:
+        idx = lower_bound(nums[i])
+        check[idx] = nums[i]
+    dp[i] = len(check)
 
 print(max(dp))
