9a536b1201ec27656750458bdc46a9922ce46220
SeungHoon Lee <lsh901673@gmail.com>
2024-03-30 15:24:31 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 44 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -5,19 +5,16 @@ input = stdin.readline
 n = int(input())
 numbers = list(map(int, input().split()))
 dp = [1] * n
+check = []
 
-for i in range(1, n):
-    for j in range(i):
-        if numbers[j] < numbers[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-# check = []
-# for i in range(n):
-#     if not check or check[-1] < numbers[i]:
-#         check.append(numbers[i])
-#     else:
-#         check[bisect_left(check, numbers[i])] = numbers[i]
-#     dp[i] = len(check)
+for i in range(n):
+    if not check or check[-1] < numbers[i]:
+        check.append(numbers[i])
+        dp[i] = len(check)
+    else:
+        idx = bisect_left(check, numbers[i])
+        check[idx] = numbers[i]
+        dp[i] = idx + 1
 
 x = max(dp)
 print(x)
