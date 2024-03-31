48adc39fb705dcb07701a36380caea87efb12676
SeungHoon Lee <lsh901673@gmail.com>
2024-03-31 12:25:22 +0900

    [Gold V] Title: 선 긋기, Time: 2244 ms, Memory: 183928 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -4,15 +4,19 @@ input = stdin.readline
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 lines.sort(key=lambda line: line[0])
-
 start, end = lines[0]
 total = 0
 
-for x, y in lines[1:]:
+for i in range(1, n):
+    x, y = lines[i]
+    
+    if end >= y:
+        continue
+
     if end < x:
         total += end - start
         start, end = x, y
-    elif end < y:
+    else:
         end = y
 
 total += end - start
