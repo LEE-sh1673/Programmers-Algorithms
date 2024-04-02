060ed87bd685f29f1ba8ae1c6202b3309b52a363
SeungHoon Lee <lsh901673@gmail.com>
2024-04-02 20:39:22 +0900

    [Silver IV] Title: 수 찾기, Time: 212 ms, Memory: 48772 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,17 +1,13 @@
 from sys import stdin
-from bisect import bisect_right
+from bisect import bisect_left
 
 
-def binary_search(a, x):
-    i = bisect_right(a, x)
-    if i != 0 and a[i - 1] == x:
-        return True
-    else:
-        return False
+def exists(nums, target):
+    return (idx := bisect_left(A, target)) != len(nums) and A[idx] == target
 
 
 N = int(stdin.readline())
 A = sorted(list(map(int, stdin.readline().split())))
 M = int(stdin.readline())
 B = list(map(int, stdin.readline().split()))
-print('\n'.join(['1' if binary_search(A, num) else '0' for num in B]))
+print('\n'.join(['1' if exists(A, num) else '0' for num in B]))
