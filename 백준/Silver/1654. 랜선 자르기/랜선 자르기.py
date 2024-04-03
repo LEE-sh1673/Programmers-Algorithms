66bf28ca38306eab1960ffc869fd08201aced0e0
SeungHoon Lee <lsh901673@gmail.com>
2024-04-03 23:25:25 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -3,22 +3,18 @@ from sys import stdin
 input = stdin.readline
 k, n = map(int, input().split())
 lines = [int(input()) for _ in range(k)]
-standard = max(lines)
-answer = 0
 
 
-def binary(low, high):
-    global n, answer
-
-    while low <= high:   
+def find(low, high):
+    while low < high:   
         mid = low + (high - low) // 2
         line_count = sum([line // mid for line in lines])
         
-        if line_count >= n:
-            answer = mid   
+        if line_count >= n:  
             low = mid + 1
         else:
-            high = mid-1
-        
-binary(0, standard * 2)
-print(answer)
+            high = mid
+    return low - 1
+
+
+print(find(0, max(lines)+1))
