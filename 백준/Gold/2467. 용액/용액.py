e29dac11ed705093f7d6d6faebaf42225248412a
SeungHoon Lee <lsh901673@gmail.com>
2024-04-07 20:21:33 +0900

    [Gold V] Title: 용액, Time: 104 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,26 +1,29 @@
 from sys import stdin
 
 input = stdin.readline
-n = int(input().strip())
-nums = list(map(int, input().strip().split()))
+n = int(input())
+juices = list(map(int, input().split()))
+min_blending = abs(juices[0] + juices[-1])
 
-def bin_search(start, end):
-    answer = nums[start], nums[end]
-    max_blending = abs(nums[start] + nums[end])
+
+def search(start, end):
+    ans_x, ans_y = 0, n-1
+    min_blending = abs(juices[start] + juices[end])
 
     while start < end:
-        blending = nums[start] + nums[end]
+        blending = juices[start] + juices[end]
 
-        if abs(blending) < max_blending:
-            max_blending = abs(blending)
-            answer = nums[start], nums[end]
-    
+        if min_blending > abs(blending):
+            min_blending = abs(blending)
+            ans_x, ans_y = start, end
+
         if blending < 0:
             start += 1
         else:
             end -= 1
-            
-    return answer
-    
 
-print(*bin_search(0, n-1))
+    return ans_x, ans_y
+
+
+ans_x, ans_y = search(0, n-1)
+print(juices[ans_x], juices[ans_y])
