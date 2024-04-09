19fa63c989c0a902771378166885593a871d0878
SeungHoon Lee <lsh901673@gmail.com>
2024-04-09 21:28:51 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 432 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,43 +1,51 @@
-import sys
+from sys import stdin
 
-N, M = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-directions = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+input = stdin.readline
+n, m = map(int, input().split())
+baskets = [list(map(int, input().split())) for _ in range(n)]
+moves = [tuple(map(int, input().split())) for _ in range(m)]
 
-# 9시, 10.5시, 12시, 1.5시, 3시, 4.5시, 6시, 7.5시
-# d ix -1 해줘야 됨.
-# d의 대각선 : 1, 3, 5, 7
-rx = [0, -1, -1, -1, 0, 1, 1, 1]
-ry = [-1, -1, 0, 1, 1, 1, 0, -1]
-clouds = [(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)]
-for direction in directions:
-    d, m = direction[0] - 1, direction[1] % N
-    not_cloud = set()
+dx8 = ("E", 0, -1, -1, -1, 0, 1, 1, 1)
+dy8 = ("E", -1, -1, 0, 1, 1, 1, 0, -1)
+dx4 = (-1, -1, 1, 1)
+dy4 = (-1, 1, -1, 1)
+
+clouds = [
+    [n - 1, 0], [n - 1, 1],
+    [n - 2, 0], [n - 2, 1]
+]
+
+for direction, steps in moves:
+    moved_clouds = set()
+
     while clouds:
         x, y = clouds.pop()
-        nx, ny = (x + m*rx[d]) % N, (y + m*ry[d]) % N
-        graph[nx][ny] += 1
-        # 구름이 있던 자리는 저장해뒀다가 나중에 제외 시켜줌
-        not_cloud.add((nx, ny))
+        dx = (x + steps * dx8[direction]) % n
+        dy = (y + steps * dy8[direction]) % n
+        baskets[dx][dy] += 1
+        moved_clouds.add((dx, dy))
 
-    # 물 뿌리기 (주의) 이 행동은 먼저 비가 내린 뒤 진행해야됨.
-    for nx, ny in not_cloud:
-        count = 0
-        for _ in range(1, 8, 2):
-            nnx = nx + rx[_]
-            nny = ny + ry[_]
-            if 0 <= nnx < N and 0 <= nny < N:
-                if graph[nnx][nny]:
-                    count += 1
-        graph[nx][ny] += count
-    # 새로운 구름 생성
-    for x in range(N):
-        for y in range(N):
-            # 기존 구름 있던 자리가 아니고
-            if (x, y) not in not_cloud:
-                # 물의 양이 2 이상인 곳 구름 생성 후 물 양 줄이기
-                if graph[x][y] >= 2:
-                    clouds.append((x, y))
-                    graph[x][y] -= 2
+    # 각 구름의 좌표를 (행, 열) 오름차 순 기준으로 순서대로 좌우 대각선을 비교하여 1 이상인 값의 횟수를 구한다. (N)
+    for x, y in moved_clouds:
+        water_count = 0
+        for i in range(4):
+            dx = x + dx4[i]
+            dy = y + dy4[i]
 
-print(sum([sum(_) for _ in graph]))
+            if dx < 0 or dx > n - 1 or dy < 0 or dy > n - 1:
+                continue
+
+            if baskets[dx][dy] > 0:
+                water_count += 1
+
+        baskets[x][y] += water_count
+
+    # cloud 좌표에 해당하는 값을 제외한 필드를 순회하여 값이 2 이상인 값을 새로 클라우드에 담는다. (N^2)
+    for i in range(n):
+        for j in range(n):
+            if (i, j) in moved_clouds or baskets[i][j] < 2:
+                continue
+            baskets[i][j] -= 2
+            clouds.append((i, j))
+
+print(sum([sum(_) for _ in baskets]))
