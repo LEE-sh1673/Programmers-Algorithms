9916495e16eb9a8481b58982a1ab1e94ad79b4e5
SeungHoon Lee <lsh901673@gmail.com>
2024-04-24 12:42:05 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 816 ms, Memory: 155560 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,15 +1,32 @@
 from sys import stdin
-from bisect import bisect_left
 
 input = stdin.readline
 n = int(input())
-numbers = list(map(int, input().split()))
-check = []
+A = list(map(int, input().split()))
 
-for i in range(n):
-    if not check or check[-1] < numbers[i]:
-        check.append(numbers[i])
-    else:
-        check[bisect_left(check, numbers[i])] = numbers[i]
 
-print(len(check))
+def lower_bound(arr, target):
+    s, e = 0, len(arr) - 1
+    while s < e:
+        mid = s + (e-s) // 2
+
+        # [10, 15, 15, 20] <- 15? -> return '1'
+        if arr[mid] < target:
+            s = mid + 1
+        else:
+            e = mid
+    return s
+
+
+def second_solution(A, n):
+    from bisect import bisect_left
+    check = []
+    for i in range(n):
+        if check and check[-1] >= A[i]:
+            check[bisect_left(check, A[i])] = A[i]
+        else:
+            check.append(A[i])
+    return len(check)
+
+
+print(second_solution(A, n))
