0f4a06630887d9e480250ce34b0ad9089ff46635
SeungHoon Lee <lsh901673@gmail.com>
2024-05-04 12:06:33 +0900

    [Gold IV] Title: 호텔, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -2,11 +2,11 @@ from sys import maxsize
 
 C, N = map(int, input().split())
 costs = [tuple(map(int, input().split())) for _ in range(N)]
-dp = [maxsize] * 1101
+dp = [maxsize] * 1100
 dp[0] = 0
 
 for cost, cnt in costs:
-    for i in range(cnt, 1101):
+    for i in range(cnt, 1100):
         dp[i] = min(dp[i-cnt] + cost, dp[i])
 
 print(min(dp[C:]))
