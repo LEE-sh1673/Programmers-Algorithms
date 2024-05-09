7b4796cf66382d6484200ff475d82c5f0083f74c
SeungHoon Lee <lsh901673@gmail.com>
2024-05-09 21:22:08 +0900

    [Gold IV] Title: 불!, Time: 2764 ms, Memory: 63684 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -9,27 +9,21 @@ dy = (0, 1, 0, -1)
 R, C = map(int, input().split())
 board = [list(input().strip()) for _ in range(R)]
 
-f_dist = [[0] * C for _ in range(R)]
-j_dist = [[0] * C for _ in range(R)]
-
-fires = deque()
-js = deque()
+dist = [[0] * C for _ in range(R)]
+que = deque()
 
 for i in range(R):
     for j in range(C):
-        f_dist[i][j] = -1
-        j_dist[i][j] = -1
+        dist[i][j] = -1
 
         if board[i][j] == 'F':
-            f_dist[i][j] = 0
-            fires.append((i, j))
-
+            dist[i][j] = 0
+            que.append((i, j))
         elif board[i][j] == 'J':
-            j_dist[i][j] = 0
-            js.append((i, j))
+            J = (i, j)
 
-while fires:
-    x, y = fires.popleft()
+while que:
+    x, y = que.popleft()
 
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
@@ -37,39 +31,36 @@ while fires:
         if 0 > nx or 0 > ny or nx >= R or ny >= C:
             continue
 
-        if board[nx][ny] == '#' or f_dist[nx][ny] >= 0:
+        if board[nx][ny] == '#' or dist[nx][ny] != -1:
             continue
 
-        f_dist[nx][ny] = f_dist[x][y] + 1
-        fires.append((nx, ny))
+        dist[nx][ny] = dist[x][y] + 1
+        que.append((nx, ny))
 
-"""
--1 -1 -1 -1 
--1 1 0 -1 
--1 2 1 -1 
--1 3 2 -1
-"""
-answer = -1
+x, y = J
+dist[x][y] = 0
+que.append((x, y))
+answer = 0
 is_exit = False
 
-while js and not is_exit:
-    x, y = js.popleft()
+while que and not is_exit:
+    x, y = que.popleft()
 
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
 
         if 0 > nx or 0 > ny or nx >= R or ny >= C:
-            answer = j_dist[x][y] + 1
+            answer = dist[x][y] + 1
             is_exit = True
             break
 
-        if board[nx][ny] == '#' or j_dist[nx][ny] >= 0:
+        if board[nx][ny] == '#':
             continue
 
-        if 0 <= f_dist[nx][ny] <= j_dist[x][y] + 1:
+        if dist[nx][ny] != -1 and dist[nx][ny] <= dist[x][y] + 1:
             continue
 
-        j_dist[nx][ny] = j_dist[x][y] + 1
-        js.append((nx, ny))
+        dist[nx][ny] = dist[x][y] + 1
+        que.append((nx, ny))
 
 print('IMPOSSIBLE' if not is_exit else answer)
