6a52458898a36dbd40703e6ce08edaa82131f542
SeungHoon Lee <lsh901673@gmail.com>
2024-05-15 17:33:57 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 620 ms, Memory: 112252 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -5,38 +5,38 @@ n, m, r = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 
-def rot90_anticlockwise(arr, times):
-    rotated = arr[::]
-    for _ in range(times):
-        rotated = rotated[1:] + [rotated[0]]
-    return rotated
+def rotate(i):
+    # (0, 0) -> (1, 1)
+    temp = board[i][i]
+
+    # 1. 위 (왼 <- 오) : 2 3 4 4
+    for j in range(i, m - i - 1):
+        board[i][j] = board[i][j + 1]
+
+    # 2. 오 (위 <- 아래): 8 12 16 16
+    for j in range(i, n - i - 1):
+        board[j][m - i - 1] = board[j + 1][m - i - 1]
+
+    # 3. 아래 (왼 -> 오): 13 13 14 15
+    for j in range(m - i - 1, i, -1):
+        board[n - i - 1][j] = board[n - i - 1][j - 1]
+
+    # 4. 왼 (위 -> 아래): 2 2 5 9
+    for j in range(n - i - 1, i, -1):
+        board[j][i] = board[j - 1][i]
+
+    # 5. 대체 (board[i+1][i] = temp)
+    board[i + 1][i] = temp
 
 
-def rotate(r):
-    arr = []
-    for i in range(min(n, m) // 2):
-        arr.clear()
-        arr += board[i][i:m - i]
-        arr += [board[j][m - i - 1] for j in range(i + 1, n - i)]
-        arr += board[n - i - 1][i:m - i - 1][::-1]
-        arr += [board[j][i] for j in range(n - i - 2, i, -1)]
-
-        arr = rot90_anticlockwise(arr, r)
-
-        for j in range(i, m - i):
-            board[i][j] = arr.pop(0)
-
-        for j in range(i + 1, n - i - 1):
-            board[j][m - i - 1] = arr.pop(0)
-
-        for j in range(m - i - 1, i - 1, -1):
-            board[n - i - 1][j] = arr.pop(0)
-
-        for j in range(n - i - 2, i, -1):
-            board[j][i] = arr.pop(0)
-
-
-rotate(r)
+"""
+total = r * n//2 * (2n + 2m - 8i + 1)
+[r = 1000, n,m = 300]
+=> 1000 * 150 * (2*300 + 2*300) = 135,150,000 + 90,000
+"""
+for time in range(r):
+    for i in range(min(n, m)//2):
+        rotate(i)
 
 for i in range(n):
     for j in range(m):
