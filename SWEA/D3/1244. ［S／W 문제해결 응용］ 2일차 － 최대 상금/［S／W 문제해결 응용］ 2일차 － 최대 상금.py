e8f4966a9b65fb4ef9c18290a21648c5ed70ba9a
SeungHoon Lee <lsh901673@gmail.com>
2024-05-19 11:05:53 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 162 ms, Memory: 50,544 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,27 +1,29 @@
-def dfs(start, cnt):
+def dfs(cnt):
     global answer
     global visited
 
-    if cnt == nums_change:
+    if cnt == change_cnt:
         answer = max(answer, int(''.join(score)))
         return
-    for i in range(len(score)):
-        for j in range(i + 1, len(score)):
-            score[i], score[j] = score[j], score[i]
-            tmp = ''.join(score)
 
-            if (tmp, cnt) not in visited:
-                visited[(tmp, cnt)] = True
-                dfs(i, cnt + 1)
-                
+    for i in range(n):
+        for j in range(i + 1, n):
+            score[i], score[j] = score[j], score[i]
+            current = ''.join(score)
+
+            if (current, cnt) not in visited:
+                visited[(current, cnt)] = True
+                dfs(cnt + 1)
+
             score[i], score[j] = score[j], score[i]
 
 
 for test_case in range(1, int(input()) + 1):
-    score, nums_change = input().split()
-    nums_change = int(nums_change)
-    score = [ch for ch in score]
+    score, change_cnt = input().split()
+    score = list(score)
+    change_cnt = int(change_cnt)
+    n = len(score)
     answer = 0
-    visited = {}
-    dfs(0, 0)
+    visited = {(''.join(score), 0): True}
+    dfs(0)
     print(f'#{test_case} {answer}')
