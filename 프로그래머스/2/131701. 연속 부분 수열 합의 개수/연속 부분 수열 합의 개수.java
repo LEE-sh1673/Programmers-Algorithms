bca0acda58285b530d809c7f49903e73264e9fc4
SeungHoon Lee <lsh901673@gmail.com>
2024-06-07 23:08:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 95.20 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -4,23 +4,14 @@ import java.util.*;
 class Solution {
     
     public int solution(int[] elements) {
-        final int[] newElements = new int[2 * elements.length];
-
-        for (int i = 0; i < newElements.length; i++) {
-            newElements[i] = elements[i % elements.length];
-        }
-
+        final int[] dp = new int[elements.length];
         final Set<Integer> numbers = new HashSet<>();
+        final int n = elements.length;
 
-        for (int i = 1; i <= elements.length; i++) {
-            for (int j = 0; j < elements.length; j++) {
-                int[] sliceNumbers = Arrays.copyOfRange(newElements, j, j + i);
-                int sum = 0;
-
-                for (int number : sliceNumbers) {
-                    sum += number;
-                }
-                numbers.add(sum);
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i < n; i++) {
+                dp[i] += elements[(len + i + 1) % n];
+                numbers.add(dp[i]);
             }
         }
         return numbers.size();
