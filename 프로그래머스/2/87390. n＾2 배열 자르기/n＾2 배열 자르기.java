f205d952d20f2c1fbab1a7f060373e3037238c31
SeungHoon Lee <lsh901673@gmail.com>
2024-06-08 13:50:39 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.69 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,9 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int n, long left, long right) {
-        int[] answer = new int[(int) (right - left + 1)];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = (int) (Math.max((left + i) / n, (left + i) % n) + 1);
-        }
-        return answer;
+        return LongStream.rangeClosed(left, right)
+                .mapToInt(v -> (int) Math.max(v / n, v % n) + 1)
+                .toArray();
     }
 }
