d6d4d6d4a406b7cf63b57f62ab68d796f3137b53
SeungHoon Lee <lsh901673@gmail.com>
2024-06-12 21:02:18 +0900

    [level 2] Title: 의상, Time: 6.33 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 66.4 MB, 시간: 5.08 ms
+메모리: 75.8 MB, 시간: 6.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 4:20:48
+2024년 06월 12일 21:02:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,35 +1,19 @@
 import java.util.*;
 import java.util.stream.*;
 
+import static java.util.stream.Collectors.*;
+
 
 class Solution {
-    public int solution(String[][] clothes) {  
-        Map<String, Long> t = Arrays.stream(clothes)
-            .map(Cloth::new)
-            .collect(Collectors.groupingBy(
-                Cloth::getType,
-                Collectors.counting()
-            ));
-
-        long t2 = t.values().stream()
-            .reduce(1L, (total, countByType) -> total * (countByType + 1));
-  
-        return (int) t2 - 1;
-    }
-    
-    private static class Cloth {
-        
-        private final String name;
-        
-        private final String type;
-        
-        Cloth(final String[] cloth) {
-            this.name = cloth[0];
-            this.type = cloth[1];
-        }
-        
-        public String getType() {
-            return type;
-        }
+    public int solution(String[][] clothes) {
+        /*
+            (A+1)*(B+1)-1 을 구현
+         */
+        return Arrays.stream(clothes)
+                .collect(Collectors.groupingBy(p -> p[1], Collectors.counting()))
+                .values()
+                .stream()
+                .reduce(1L, (total, count) -> total * (count + 1))
+                .intValue() - 1;
     }
 }
