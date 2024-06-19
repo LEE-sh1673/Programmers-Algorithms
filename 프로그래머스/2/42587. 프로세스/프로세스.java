2c306b1bdee29d429d5dd567d18798d8c6e00ec3
SeungHoon Lee <lsh901673@gmail.com>
2024-06-19 22:14:15 +0900

    [level 2] Title: 프로세스, Time: 2.68 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,66 +2,58 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
-public class Printer {
-
-    private final Queue<Document> documents;
-
-
-    private static class Document {
-
-        private final int order;
-        private final int priority;
-
-        public Document(final int order, final int priority) {
-            this.order = order;
-            this.priority = priority;
-        }
-    }
-
-    public Printer(int[] priorities) {
-        this.documents = new LinkedList<>();
-        mapDocuments(priorities);
-    }
-
-    private void mapDocuments(final int[] priorities) {
-        for (int i = 0; i < priorities.length; i++) {
-            documents.offer(new Document(i, priorities[i]));
-        }
-    }
-
-    public int getOrder(final int location) {
-        int order = 0;
-
-        while (true) {
-            Document document = documents.poll();
-
-            if (isGreaterThen(document)) {
-                documents.offer(document);
-            } else {
-                order++;
-                if (matchOrder(location, document)) {
-                    return order;
-                }
-            }
-        }
-    }
-
-
-    private static boolean matchOrder(final int location, final Document document) {
-        return document != null && document.order == location;
-    }
-
-    private boolean isGreaterThen(final Document document) {
-        return document != null && documents.stream()
-            .anyMatch(doc -> doc.priority > document.priority);
-    }
-}
-
-
-
 class Solution {
+
     public int solution(int[] priorities, int location) {
         Printer printer = new Printer(priorities);
         return printer.getOrder(location);
     }
+
+    private static class Printer {
+
+        private final Queue<Document> documents;
+
+        public Printer(final int[] priorities) {
+            this.documents = new LinkedList<>();
+            for (int i = 0; i < priorities.length; i++) {
+                documents.add(new Document(i, priorities[i]));
+            }
+        }
+
+        public int getOrder(final int location) {
+            int order = 0;
+
+            while (!documents.isEmpty()) {
+                final Document document = documents.poll();
+
+                // 우선순위가 더 높은 프로세스가 존재
+                if (!isGreatest(document.priority)) {
+                    documents.offer(document);
+                    continue;
+                }
+
+                // 대기 중인 프로세스를 꺼내는 경우
+                order++;
+                if (document.order == location) {
+                    return order;
+                }
+            }
+            return order;
+        }
+
+        private boolean isGreatest(final int priority) {
+            return documents.stream().allMatch(doc -> doc.priority <= priority);
+        }
+
+        private static class Document {
+            private final int order;
+            private final int priority;
+
+            public Document(final int order, final int priority) {
+                this.order = order;
+                this.priority = priority;
+            }
+        }
+    }
+
 }
