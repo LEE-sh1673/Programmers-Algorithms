0210302101d0ae87be9f37ff0a85e4f7b629ea1d
SeungHoon Lee <lsh901673@gmail.com>
2024-06-22 15:19:54 +0900

    [level 2] Title: 피로도, Time: 0.25 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.27 ms
+메모리: 72.5 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 21:1:24
+2024년 06월 22일 15:19:52
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,9 +1,50 @@
 class Solution {
 
+    // public int solution(int k, int[][] dungeons) {
+    //     DungeonCostCalculator calculator 
+    //         = new DungeonCostCalculator(dungeons);
+    //     return calculator.calculate(k);
+    // }
+    
     public int solution(int k, int[][] dungeons) {
-        DungeonCostCalculator calculator 
-            = new DungeonCostCalculator(dungeons);
-        return calculator.calculate(k);
+        DungeonTravelSimulator simulator = new DungeonTravelSimulator(dungeons);
+        return simulator.getMaximumTravels(k);
+    }
+    
+    private static class DungeonTravelSimulator {
+
+        private static int maximumTravels = 0;
+
+        private final int[] requires;
+        private final int[] costs;
+        private final boolean[] visited;
+
+        public DungeonTravelSimulator(final int[][] dungeons) {
+            this.requires = new int[dungeons.length];
+            this.costs = new int[dungeons.length];
+            this.visited = new boolean[dungeons.length];
+
+            for (int i = 0; i < dungeons.length; i++) {
+                this.requires[i] = dungeons[i][0];
+                this.costs[i] = dungeons[i][1];
+            }
+        }
+
+        public int getMaximumTravels(final int k) {
+            simulate(0, k);
+            return maximumTravels;
+        }
+
+        private void simulate(int count, int cost) {
+            for (int i = 0; i < requires.length; i++) {
+                if (!visited[i] && cost >= requires[i]) {
+                    visited[i] = true;
+                    simulate(count + 1, cost - costs[i]);
+                    visited[i] = false;
+                }
+            }
+            maximumTravels = Math.max(maximumTravels, count);
+        }
     }
 }
 
