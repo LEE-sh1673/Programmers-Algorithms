078c0cf93d2b5603261a29252a4e817dbc92a71a
SeungHoon Lee <lsh901673@gmail.com>
2024-09-03 00:14:13 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,8 +1,13 @@
-from functools import reduce
-
-
 def solution(clothes):
-    d = {}
-    for cloth, category in clothes:
-        d[category] = d.get(category, 0) + 1
-    return reduce(lambda x, y: x * (y + 1), d.values(), 1) - 1
+    from collections import Counter
+
+    # 모든 경우의 수 => (a+1) * (b+1) * ... * (n+1) - 1
+    # e.g.
+    # (a+1)*(b+1)*(c+1)-1 => 7
+    # a, b, c, ab, bc, ca, abc
+    
+    c_cnt = Counter([kind for _, kind in clothes])
+    answer = 1
+    for v in c_cnt.values():
+        answer *= (v+1)
+    return answer - 1
