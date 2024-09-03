6f75fd4b9aa7cdfe8cfc2d3135b2940e09abb29d
SeungHoon Lee <lsh901673@gmail.com>
2024-09-03 22:07:42 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,11 +1,43 @@
+"""
+
+1. 각 장르별 분류
+    - classic => 0, 2, 3 => [500, 0], [150, 2], [800, 3]
+    - pop => 1, 4  => [600, 1], [2500, 4]
+2. 재생 횟수를 기준으로 내림차 순 정렬한다.
+    - [800, 3], [500, 0], ..
+    - ...
+3. 각 장르별로 두 개씩 가져온다.
+    - [800, 3], [500, 0]
+    - [2500, 4], [600, 1]
+4. 각 장르별로 선별된 곡을 집어넣는다. 이때 가장 많이 재생된 장르를 먼저 삽입한다.
+    - [800, 3], [500, 0] => 1,300
+    - [2500, 4], [600, 1] => 3,100
+    - [4, 1, 3, 0]
+"""
 def solution(genres, plays):
-    d = {e:[] for e in set(genres)}
-    for e in zip(genres, plays, range(len(plays))):
-        d[e[0]].append([e[1], e[2]])
+    from collections import defaultdict
     
-    genreSort = sorted(list(d.keys()), key= lambda genre: sum(map(lambda song_info: song_info[0], d[genre])), reverse = True)
+    gs = defaultdict(list)
+    for no, gp in enumerate(zip(genres, plays)):
+        genre, play = gp
+        gs[genre].append([play, no])
+    
+    for genre in gs.keys():
+        gs[genre].sort(key=lambda item : (-item[0], item[1]))
+    
+    candidates = [gs[genre] for genre in gs.keys()]
+    candidates.sort(key=comp)
+    
     answer = []
-    for genre in genreSort:
-        sorted_info = [song_info[1] for song_info in sorted(d[genre], key= lambda song: (song[0], -song[1]), reverse = True)]
-        answer += sorted_info[:min(len(sorted_info), 2)]
+    for candidate in candidates:
+        answer += [no for _, no in candidate[:2]]
     return answer
+
+def comp(item):
+    total_plays = 0
+    min_no = 1e10
+    
+    for plays, no in item:
+        total_plays += plays
+        min_no = min(min_no, no)
+    return -total_plays, min_no
