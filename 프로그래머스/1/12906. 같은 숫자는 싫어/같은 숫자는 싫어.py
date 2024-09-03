b1b1b68d033ec612602cf5f070a5f2f989e7f01a
SeungHoon Lee <lsh901673@gmail.com>
2024-09-03 22:10:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 61.99 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,8 @@
 def solution(arr):
     stk = []
-    
     for el in arr:
         if stk and stk[-1] == el:
             continue
-        stk.append(el)
-    
+        else:
+            stk.append(el)
     return stk
