679a1619890b3936d64a680f3c55e36ea0d86c90
SeungHoon Lee <lsh901673@gmail.com>
2024-09-04 23:48:21 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 51.98 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,19 +1,20 @@
 def solution(bridge_length, weight, truck_weights):
-    bridge = [0] * bridge_length
-    total_weight = 0
+    from collections import deque
+    
+    bridges = deque([0] * bridge_length)
+    total_weights = 0
     step = 0
     
     while truck_weights:
-        total_weight -= bridge.pop(0)
+        total_weights -= bridges.popleft()
         
-        if total_weight + truck_weights[0] > weight:
-            bridge.append(0)
+        if total_weights + truck_weights[0] > weight:
+            bridges.append(0)
         else:
-            truck = truck_weights.pop(0)
-            bridge.append(truck)
-            total_weight += truck
+            truck_weight = truck_weights.pop(0)
+            total_weights += truck_weight
+            bridges.append(truck_weight)
+            
         step += 1
-
-    # 이전 단계까지 하여 마지막 트럭이 다리 위에 올라간 상태이다.
-    # 따라서 마지막 트럭이 다리를 끝까지 건너는 시간(즉 bridge_length)를 더하면 답이다.
+        
     return step + bridge_length
