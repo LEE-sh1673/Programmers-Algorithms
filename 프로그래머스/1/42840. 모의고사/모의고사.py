df231ded1f5dfc5b1940581b087004f8933dcbfa
SeungHoon Lee <lsh901673@gmail.com>
2024-09-06 22:54:02 +0900

    [level 1] Title: 모의고사, Time: 3.71 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,16 @@
-def solution(answers):
-    answer = []
-    n = len(answers)
-    
-    u1 = [1,2,3,4,5] * n
-    u2 = [2,1,2,3,2,4,2,5] * n
-    u3 = [3,3,1,1,2,2,4,4,5,5] * n
+def solution(answers):    
+    u1 = [1,2,3,4,5]
+    u2 = [2,1,2,3,2,4,2,5]
+    u3 = [3,3,1,1,2,2,4,4,5,5]
     cnts = [0] * 3
+    answer = []
     
-    for i in range(n):
-        ans = answers[i]
-        
-        if ans == u1[i]:
+    for i, ans in enumerate(answers):        
+        if ans == u1[i % 5]:
             cnts[0] += 1
-        if ans == u2[i]:
+        if ans == u2[i % 8]:
             cnts[1] += 1
-        if ans == u3[i]:
+        if ans == u3[i % 10]:
             cnts[2] += 1
     
     return [idx+1 for idx, el in enumerate(cnts) if el == max(cnts)]
