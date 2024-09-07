3936f5b0892fd851cda94c94a1f1a863422eb346
SeungHoon Lee <lsh901673@gmail.com>
2024-09-07 12:20:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 547.40 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,35 +1,42 @@
-class PriorityQueue:
+class DblEndedPQ:
     def __init__(self):
-        self.que = set()
+        self.items = set()
     
-    def insert(self, item):        
-        self.que.add(item)
-        
-    def remove_max(self):
-        if self.que:
-            self.que.discard(max(self.que))
+    def size(self):
+        return len(self.items)
+    
+    def put(self, item):
+        self.items.add(item)
+    
+    def get_min(self):
+        return min(self.items) if self.items else 0
+
+    def get_max(self):
+        return max(self.items) if self.items else 0
     
     def remove_min(self):
-        if self.que:
-            self.que.discard(min(self.que))
-            
-    def max(self):
-        return max(self.que) if self.que else 0
+        if not self.items:
+            return
+        self.items.remove(self.get_min())
+        
+    def remove_max(self):
+        if not self.items:
+            return
+        self.items.remove(self.get_max())
 
-    def min(self):
-        return min(self.que) if self.que else 0
-
-
-def solution(operations):
-    answer = []
-    q = PriorityQueue()
     
-    for operation in operations:
-        cmd, val = operation.split()
-        
-        if cmd == 'I':
-            q.insert(int(val))
-        elif cmd == 'D':
-            q.remove_max() if val == '1' else q.remove_min()
-        
-    return [q.max(), q.min()]
+def solution(operations):
+    heap = DblEndedPQ()
+    
+    for cv in operations:
+        if cv == 'D 1':
+            heap.remove_max()
+        elif cv == 'D -1':
+            heap.remove_min()       
+        else:
+            heap.put(int(cv[2:]))
+            
+    return [heap.get_max(), heap.get_min()]
+
+
+
